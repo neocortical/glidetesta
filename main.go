70f791e15a2d2b11a5567204232cf06ce7e5628c
Nathan Smith <nate@nathan@neocortical.net>
2016-08-03 18:17:01 -0700

    glidetesta code complete

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/neocortical/glidetestb"
+)
+
+func main() {
+
+	fmt.Println("Running...")
+
+	fmt.Println(glidetestb.GlideTestB())
+
+	fmt.Println("...done.")
+}
